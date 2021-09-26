2de2eaaf296ecf0f9ecc13c9d40877d984842c10
amirrezasadeqi <e.a.sadeqi@gmail.com>
2021-09-26 15:39:27 +0330

    Update nvimtree_configs base on new setup function of its repository.

--- lua/nvimtree_configs.lua
+++ lua/nvimtree_configs.lua
@@ -1,11 +1,16 @@
-vim.g.nvim_tree_auto_open = 1
-vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' }
+require('nvim-tree').setup {
+  open_on_setup = true,
+  ignore_ft_on_setup = {'startify', 'dashboard'},
+  --[[ When open nvim with no directory and open a project with telescope project plugin
+  you need to toggle nvim tree twice to referesh the project tree but if you set 
+  bellow options it will be ok. but i will leave them commented because the default
+  is zero for bellow options and maybe cause some wierd behaviors in some situations
+  (I didn't see any bad behavior yet!) and it is not annoying now. in future ... ]]
+  -- update_cwd = true, -- I moved this option into setup function. the other option
+                        -- is outside of setup function and is "nvim_tree_respect_buf_cwd".
+}
 
--- When open nvim with no directory and open a project with telescope project plugin
--- you need to toggle nvim tree twice to referesh the project tree but if you set 
--- bellow options it will be ok. but i will leave them commented because the default
--- is zero for bellow options and maybe cause some wierd behaviors in some situations
--- (I didn't see any bad behavior yet!) and it is not annoying now. in future ...
+-- vim.g.nvim_tree_respect_buf_cwd = 1
 
---[[ vim.g.nvim_tree_update_cwd = 1
-vim.g.nvim_tree_respect_buf_cwd = 1 ]]
+
+

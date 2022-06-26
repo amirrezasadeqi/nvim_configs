94cbff6c9eea6090bd7c5673459e953693850430
amirrezasadeqi <e.a.sadeqi@gmail.com>
2022-06-26 20:15:30 +0430

    The First Step to create a nice tuned neovim configuration in pure Lua.

--- after/queries/cpp/textobjects.scm
+++
@@ -1,6 +0,0 @@
-; These custom queries/captures will be added to the default ones which are
-; defined in nvim-treesitter-textobjects plugin.
-
-(function_definition
-  declarator: (function_declarator
-    declarator: (identifier) @function.name))
--- task_scripts/bash_scripts/ros_workspace_init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# ~/.config/nvim/task_scripts/bash_scripts/ros_workspace_init.sh
-
-
-mkdir src && cd src && catkin_init_workspace && cd .. && catkin_make && shell=$(printenv SHELL) && if [ ${shell#/bin/} == "zsh" ]; then source devel/setup.zsh ; else source devel/setup.bash ; fi
-

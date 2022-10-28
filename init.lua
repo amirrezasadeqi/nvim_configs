256a9115a2741d1dd08adf97bc44616e6e183b95
amirrezasadeqi <e.a.sadeqi@gmail.com>
2022-10-28 19:46:11 +0330

    update_capabilities( deprecated ) -> default_capabilities folke/lua-dev.nvim -> folke/neodev.nvim and some auto-formats

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ require("mnvim.plugins")
 -- Highlight on yank
 local yankGrp = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-  command = "silent! lua vim.highlight.on_yank()",
-  group = yankGrp,
+	command = "silent! lua vim.highlight.on_yank()",
+	group = yankGrp,
 })
